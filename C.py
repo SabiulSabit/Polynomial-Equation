dd2ac5593322a1e078ec0c2f3d682045155bc6e8
S. M. Sabiul Hajjaj <39924795+SabiulSabit@users.noreply.github.com>
2020-07-09 17:17:49 +0600

    update input format

--- C.py
+++ C.py
@@ -1,23 +1,29 @@
 import numpy as np
 
+
 def cal():
     print("Enter 6 coefs, first must be  >  0:")
-    number_array = list()
-    number =6
-    for i in range(int(number)):
-        n = input("")
-        number_array.append(int(n))
-    j=5        
+    #number_array = list()
+    number = 6
+
+    while (True):
+        number_array = list(map(float, input("").split()))
+        if (len(number_array) == 6):
+            break
+        else:
+            print("Wrong input. Please enter 6 ploy5 coefficients: ")
+    j = 5
     for i in number_array:
-        print("a",j," = %.3f"%i)
-        j=j-1
-        
+        print("a", j, " = %.3f" % i)
+        j = j - 1
+
     a = np.roots(number_array)
     reversed_arr = a[::-1]
 
     print("Solutions are: ")
     for i in reversed_arr:
-        k = "%.6f + %.6fi"%( i.real,i.imag)
+        k = "%.6f + %.6fi" % (i.real, i.imag)
         print(k)
-        
+
+
 cal()

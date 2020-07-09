05b74eac5b88184303c17c9c7383299c0502c66e
Anika <32257753+anika107@users.noreply.github.com>
2020-07-09 17:32:57 +0600

    Update A.py

--- A.py
+++ A.py
@@ -1,5 +1,9 @@
 import numpy as np
 
+
+#print("done")
+#number_array = [720, -612, -500, 449, 52, -60]
+
 print("Enter 6 coefs, first must be  >  0:")
 #number_array = list()
 number = 6
@@ -12,7 +16,6 @@ while (True):
         break
     else:
         print("Wrong input. Please enter 6 ploy5 coefficients: ")
-#print("done")
 
 print("a5 = ", number_array[0])
 print("a4 = ", number_array[1])
@@ -21,22 +24,32 @@ print("a2 = ", number_array[3])
 print("a1 = ", number_array[4])
 print("a0 = ", number_array[5])
 
-low = -1000000000
-high = 100000000000
 p = np.poly1d(number_array)
 
-while((high-low) >= 0.000000000000001):
+k = 0
+while (p(2**k) <= 0):
+    k = k + 1
 
-    #print("inside")
+high = 2**k
+#print("high = ", high)
+
+
+k = 0
+while (p((2 ** k)*(-1)) >= 0):
+    k = k + 1
+
+low = (2 ** k)*(-1)
+#print("low = ", low)
+
+
+while((high - low) >= 0.0000000001):
     mid = (high+low)/2
-    if(p(mid) == 0):
-        print(mid)
-        break
-    elif(p(mid)*p(low) < 0):
+    if(p(mid)*p(low) < 0):
         high = mid
     else:
         low = mid
 
+#print("low = ", low, "high = ", high)
 print("ploy5(", round(low, 6), ") = ", 0.000000)
 print(" ")
 print("Hence one real root is: x* = ", round(low, 6))
